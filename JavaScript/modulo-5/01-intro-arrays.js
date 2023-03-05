6abd43168485a026a4b647e15c7f671818044862
Roberto <robertosanchezsantos89@gmail.com>
2023-03-04 19:49:03 -0500

    iniciando arreglos

---
+++ JavaScript/modulo-5/01-intro-arrays.js
@@ -0,0 +1,10 @@
+const mascotas = ["Koen", "Muñeca", "Azabache", "atena", "rochelle"];
+const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+
+// console.log(meses[9]);
+// console.log(meses.length);
+for (let index = 0; index < meses.length; index++) {
+    const element = meses[index];
+    console.log(element);
+}
